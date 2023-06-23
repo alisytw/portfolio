428d5a046c2cf7f2195910a2fe747019d0df6b0c
alisytw <128714027+alisytw@users.noreply.github.com>
2023-06-23 13:34:13 +0300

    update
    Working navigation buttons

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-// highlight selected navigation button
-const navBtnHome = document.getElementById('nav-btn-home');
-const navBtnList = document.getElementsByTagName('a')
-for (let i = 0; i < navBtnList.length; i++) {
-    const navBtn = navBtnList[i];
-    navBtn.addEventListener('click', () => {
-        const highlitedButton = document.getElementsByClassName('nav-bar--btn__selected')
-        for (let j = 0; j < highlitedButton.length; j++) {
-            const element = highlitedButton[j];
-            element.classList.remove('nav-bar--btn__selected')
-        }
-        navBtn.classList.toggle('nav-bar--btn__selected')
-    })
+// GLOBAL VARIABLES
+// Sections
+const homeSection = document.getElementById('home')
+const projectsSection = document.getElementById('projects')
+const studyCasesSection = document.getElementById('studyCases')
+const contactSection = document.getElementById('contact')
+// Buttons
+const homeBtn = document.getElementById('nav-btn-home')
+const projectsBtn = document.getElementById('nav-btn-projects')
+const studyCasesBtn = document.getElementById('nav-btn-study-cases')
+const contactBtn = document.getElementById('nav-btn-contact')
+
+// GLOBAL FUCNTION
+function resetHighlight() {
+    const highlitedButton = document.getElementsByClassName('nav-bar--btn__selected')
+    for (let j = 0; j < highlitedButton.length; j++) {
+        const element = highlitedButton[j];
+        element.classList.remove('nav-bar--btn__selected')
+    }
 }
-if (document.URL.includes('/')) {
-    navBtnHome.classList.add('nav-bar--btn__selected')
+
+function checkHighlight() {
+    if (isInViewport(homeSection)) {
+        resetHighlight()
+        homeBtn.classList.add('nav-bar--btn__selected')
+    } else if (isInViewport(projectsSection)) {
+        resetHighlight()
+        projectsBtn.classList.add('nav-bar--btn__selected')
+    } else if (isInViewport(studyCasesSection)) {
+        resetHighlight()
+        studyCasesBtn.classList.add('nav-bar--btn__selected')
+    } else if (isInViewport(contactSection)) {
+        resetHighlight()
+        contactBtn.classList.add('nav-bar--btn__selected')
+    }
 }
+
 
 // tint card image on hover
 const cardList = document.getElementsByClassName('card--wrapper')
@@ -30,12 +50,30 @@ for (let i = 0; i < cardList.length; i++) {
     })
 }
 
-// navigation bar on scroll
-// window.onscroll = function() {navBarOnScroll()};
-// function navBarOnScroll() {
-//   if (document.body.scrollTop > 160 || document.documentElement.scrollTop > 160) {
-//     document.getElementById('navBar').style.position = 'fixed';
-//   } else {
-//     document.getElementById('navBar').style.position = 'absolute';
-//   }
-// }
+// smooth scrolling when hitting a specific button (<a> href)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// smart navigation bar highlights
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top + (rect.height / 2) > 0 && // top
+        rect.left + (rect.width / 2) > 0 && // left
+        rect.top + (rect.height / 2) < (window.innerHeight || document.documentElement.clientHeight) && // bottom
+        rect.left + (rect.width / 2) < (window.innerWidth || document.documentElement.clientWidth) // right
+    );
+}
+checkHighlight() // to highlight on launch
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 0) {
+        checkHighlight()
+    }
+}
